3540967ce0d20e715aa83c5a09b1afbc426becdd
Baumgartner_Lukas <baumgartner.webtree@gmail.com>
2018-07-23 10:39:59 +0200

    Used sdk provided location api to get user location via Network

--- app/src/main/java/com/baumgartner/wy/pt_locationawareness/MainActivity.java
+++ app/src/main/java/com/baumgartner/wy/pt_locationawareness/MainActivity.java
@@ -1,24 +1,20 @@
 package com.baumgartner.wy.pt_locationawareness;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.baumgartner.wy.pt_locationawareness.location.LocationTracker;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     public static final int PERMISSION_REQUEST_CODE = 255;
 
     private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
     private Location mLocation;
 
     @BindView(R.id.latitudeLabel)
@@ -43,52 +41,79 @@ public class MainActivity extends AppCompatActivity {
     ImageView mRefreshImageView;
 
     @Override
+    @SuppressWarnings("MissingPermission")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if(!checkPermission()){
+        if (!hasPermission()) {
             requestPermissions();
-        }else{
-            retrieveLastLocation();
+        } else {
+
+            LocationTracker locationTracker = new LocationTracker(this);
+            mLocation = locationTracker.getLocation();
+            if(mLocation == null){
+                mLatitude.setText("-1");
+                mLongitude.setText("-1");
+            }else {
+                mLatitude.setText(mLocation.getLatitude() + "");
+                mLongitude.setText(mLocation.getLongitude() + "");
+            }
+//            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+//            mLocationRequest = new LocationRequest()
+//                    .setSmallestDisplacement(1000f)
+//                    .setInterval(10 * 60 * 1000)
+//                    .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+//            mLocationCallback = new LocationCallback() {
+//                @Override
+//                public void onLocationResult(LocationResult result) {
+//                    mLatitude.setText(result.getLastLocation().getLatitude() + "");
+//                    mLongitude.setText(result.getLastLocation().getLongitude() + "");
+//                }
+//            };
+//            mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
         }
     }
 
-    @SuppressWarnings("MissingPermission")
-    private void retrieveLastLocation() {
-        mFusedLocationClient.getLastLocation()
-                .addOnCompleteListener(this, new OnCompleteListener<Location>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Location> task) {
-                        if(task.isSuccessful() && task.getResult() != null){
-                            mLocation = task.getResult();
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        if (!hasPermission()) {
+//            requestPermissions();
+//        } else {
+//            updateLocation();
+//        }
+//    }
 
-                            mLatitude.setText("" + mLocation.getLatitude());
-                            mLongitude.setText("" + mLocation.getLongitude());
-                        }else{
-                            return;
-                        }
-                    }
-                });
-    }
+//    @SuppressWarnings("MissingPermission")
+//    private void updateLocation() {
+//        mFusedLocationClient.getLastLocation()
+//                .addOnCompleteListener(this, new OnCompleteListener<Location>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Location> task) {
+//                        if(task.isSuccessful() && task.getResult() != null){
+//                            mLocation = task.getResult();
+//
+//                            mLatitude.setText("" + mLocation.getLatitude());
+//                            mLongitude.setText("" + mLocation.getLongitude());
+//                        }else{
+//                            return;
+//                        }
+//                    }
+//                });
+//    }
 
     private void requestPermissions() {
         ActivityCompat.requestPermissions(MainActivity.this,
-                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.INTERNET},
                 PERMISSION_REQUEST_CODE);
     }
 
-    private boolean checkPermission() {
-        int permissionState = ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION);
-        return permissionState == PackageManager.PERMISSION_GRANTED;
+    private boolean hasPermission() {
+        return ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED;
     }
 }
